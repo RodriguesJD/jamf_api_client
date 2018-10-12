84891a8c7d0ecd8955e99ab9aacdaf29892886b7
Jeremy Rodrigues <jeremy.rodrigues@BB3745-M110031.local>
2018-10-12 10:08:50 -0700

    Lets Start

---
+++ src/tests/test_trr_jamf.py
@@ -0,0 +1,125 @@
+from src import trr_jamf
+
+
+# https://developer.jamf.com/apis/jamf-pro-api/index
+
+
+def test_trr_jamf():
+    assert trr_jamf.trr_jamf('/computergroups').status_code == 200
+    assert trr_jamf.trr_jamf('not_a_url').status_code == 404
+    assert isinstance(trr_jamf.trr_jamf('/sites').json(), dict)
+    assert str(type(trr_jamf.trr_jamf('/sites'))) == "<class 'requests.models.Response'>"
+
+
+def test_computergroups():
+    assert trr_jamf.computergroups().status_code == 200
+    assert trr_jamf.trr_jamf('/computergroups').status_code == 200
+    assert isinstance(trr_jamf.computergroups().json(), dict)
+    assert str(type(trr_jamf.computergroups())) == "<class 'requests.models.Response'>"
+
+
+def test_computergroup():
+    assert trr_jamf.trr_jamf('/computergroups/id/12').status_code == 200
+    assert trr_jamf.computergroup_by_id(12).status_code == 200
+    assert isinstance(trr_jamf.computergroup_by_id(12).json(), dict)
+    assert str(type(trr_jamf.computergroup_by_id(12))) == "<class 'requests.models.Response'>"
+
+
+def test_computergroup_by_name():
+    assert trr_jamf.computergroup_by_name("ESET AV missing")
+    assert trr_jamf.computergroup_by_name("ESET AV missing").status_code == 200
+    assert isinstance(trr_jamf.computergroup_by_name("ESET AV missing").json(), dict)
+    assert trr_jamf.computergroup_by_name("wrong group name") is None
+    assert str(type(trr_jamf.computergroup_by_name("ESET AV missing"))) == "<class 'requests.models.Response'>"
+
+
+def test_computers():
+    assert trr_jamf.computers().status_code == 200
+    assert isinstance(trr_jamf.trr_jamf('/computers').json(), dict)
+    assert str(type(trr_jamf.trr_jamf('/computers'))) == "<class 'requests.models.Response'>"
+
+
+def test_computer():
+    assert trr_jamf.computer_by_id(20).status_code == 200
+    assert trr_jamf.computer_by_id("not an int").status_code == 404
+    assert isinstance(trr_jamf.computer_by_id(20).json(), dict)
+    assert str(type(trr_jamf.computer_by_id(20))) == "<class 'requests.models.Response'>"
+
+
+def test_computer_management():
+    assert trr_jamf.trr_jamf('/computermanagement/id/20').status_code == 200
+    assert trr_jamf.trr_jamf('not a url string').status_code == 404
+    assert isinstance(trr_jamf.computer_management(20).json(), dict)
+    assert str(type(trr_jamf.computer_management(20))) == "<class 'requests.models.Response'>"
+
+
+def test_directorybindings():
+    assert trr_jamf.trr_jamf('/directorybindings').status_code == 200
+    assert trr_jamf.directorybindings().status_code == 200
+    assert isinstance(trr_jamf.directorybindings().json(), dict)
+    assert str(type(trr_jamf.directorybindings())) == "<class 'requests.models.Response'>"
+
+
+def test_directorybinding():
+    assert trr_jamf.trr_jamf('/directorybindings/id/10').status_code == 200
+    assert trr_jamf.directorybinding(10).status_code == 200
+    assert trr_jamf.directorybinding('not an int').status_code == 404
+    assert isinstance(trr_jamf.directorybinding(10).json(), dict)
+    assert str(type(trr_jamf.directorybinding(10))) == "<class 'requests.models.Response'>"
+
+
+def test_directorybinding_by_name():
+    trr_buildings = trr_jamf.buildings().json()["buildings"]
+    for site in trr_buildings:
+        builing_bind = f"{site['name']} Directory Binding"
+        if builing_bind =='Remote Directory Binding':
+            pass
+        else:
+            assert trr_jamf.directorybinding_by_name(builing_bind)
+            assert trr_jamf.directorybinding_by_name(builing_bind).status_code == 200
+            assert isinstance(trr_jamf.directorybinding_by_name(builing_bind).json(), dict)
+            assert str(type(trr_jamf.directorybinding_by_name(builing_bind))) == "<class 'requests.models.Response'>"
+            assert trr_jamf.directorybinding_by_name("not a site name") is None
+
+
+def test_buildings():
+    assert trr_jamf.buildings().status_code == 200
+    assert isinstance(trr_jamf.trr_jamf('/buildings').json(), dict)
+    assert str(type(trr_jamf.trr_jamf('/buildings'))) == "<class 'requests.models.Response'>"
+
+
+def test_policies():
+    assert trr_jamf.policies().status_code == 200
+    assert isinstance(trr_jamf.trr_jamf('/policies').json(), dict)
+    assert str(type(trr_jamf.trr_jamf('/policies'))) == "<class 'requests.models.Response'>"
+
+
+def test_list_all_building_names():
+    assert isinstance(trr_jamf.list_all_building_names(), list)
+    for building_name in trr_jamf.list_all_building_names():
+        assert isinstance(building_name, str)
+
+def test_list_all_building_ids():
+    assert isinstance(trr_jamf.list_all_building_ids(), list)
+    for building_id in trr_jamf.list_all_building_ids():
+        assert isinstance(building_id, int)
+
+
+def test_list_all_policy_names():
+    assert isinstance(trr_jamf.list_all_policy_names(), list)
+    for policy_name in trr_jamf.list_all_policy_names():
+        assert isinstance(policy_name, str)
+
+
+def test_list_all_smartgroup_names():
+    assert isinstance(trr_jamf.list_all_smartgroup_names(), list)
+    for policy_name in trr_jamf.list_all_smartgroup_names():
+        assert isinstance(policy_name, str)
+
+
+def test_list_all_devices_past():
+    assert isinstance(trr_jamf.list_all_devices_past(5), list)
+
+
+def test_checked_in_exceeds_date():
+    assert isinstance(trr_jamf.checked_in_exceeds_date(trr_jamf.computer_by_id(10).json(), 10), bool)
---
+++ src/trr_jamf.py
@@ -0,0 +1,161 @@
+from requests.auth import HTTPBasicAuth
+import requests
+from src.secret.key import username, key
+from pprint import pprint
+from datetime import datetime
+# https://developer.jamf.com/apis/jamf-pro-api/index
+
+
+def trr_jamf(url: str) -> requests.models.Response:
+    """therealreal prod"""
+    print("Jamf Prod")
+    return requests.get(f'https://therealreal.jamfcloud.com/JSSResource{url}',
+                        auth=HTTPBasicAuth(username, key), headers={'Accept': 'application/json'})
+
+
+# def trr_jamf(url: str) -> requests.models.Response:
+#     """therealreal dev"""
+#     print("Jamf Dev")
+#     return requests.get(f'https://therealrealdev.jamfcloud.com/JSSResource{url}',
+#                         auth=HTTPBasicAuth(username, key), headers={'Accept': 'application/json'})
+
+
+def computergroups() -> requests.models.Response:
+    return trr_jamf('/computergroups')
+
+
+def computergroup_by_id(computergroup_id: str) -> requests.models.Response:
+    computer_group_url = f'/computergroups/id/{computergroup_id}'
+    return trr_jamf(computer_group_url)
+
+
+def computergroup_by_name(computergroup_name: str) -> requests.models.Response:
+    computer_groups = computergroups().json()['computer_groups']
+    for group in computer_groups:
+        if group['name'] == computergroup_name:
+            return computergroup_by_id(group['id'])
+
+
+def computers() -> requests.models.Response:
+    """'computers': [{'id': 225, 'name': 'NY65-M102770'}"""
+    return trr_jamf('/computers')
+
+
+def computer_by_id(computer_id: str) -> requests.models.Response:
+    computer_url = f'/computers/id/{computer_id}'
+    return trr_jamf(computer_url)
+
+
+def computer_management(computer_management_id: str) -> requests.models.Response:
+    computer_management_url = f'/computermanagement/id/{computer_management_id}'
+    return trr_jamf(computer_management_url)
+
+
+def directorybindings() -> requests.models.Response:
+    return trr_jamf('/directorybindings')
+
+
+def directorybinding(directorybinding_id: str) -> requests.models.Response:
+    directorybinding_url = f"/directorybindings/id/{directorybinding_id}"
+    return trr_jamf(directorybinding_url)
+
+
+def directorybinding_by_name(name: str) -> requests.models.Response:
+    directory_bindings = directorybindings().json()['directory_bindings']
+    for bind in directory_bindings:
+        if bind['name'] == name:
+            return directorybinding(bind["id"])
+
+
+def buildings() -> requests.models.Response:
+    return trr_jamf('/buildings')
+
+
+def policies() -> requests.models.Response:
+    return trr_jamf('/policies')
+
+
+def list_all_building_names():
+    all_building_names = []
+    building_s = buildings().json()['buildings']
+    for building in building_s:
+        all_building_names.append(building['name'])
+
+    return all_building_names
+
+
+def list_all_building_ids():
+    all_building_names = []
+    building_s = buildings().json()['buildings']
+    for building in building_s:
+        all_building_names.append(building['id'])
+
+    return all_building_names
+
+
+def list_all_policy_names():
+    all_policie_names = []
+    buildings = policies().json()['policies']
+    for building in buildings:
+        all_policie_names.append(building['name'])
+
+    return all_policie_names
+
+
+def list_all_smartgroup_names():
+    all_smartgroup_names = []
+    smartgroups = computergroups().json()['computer_groups']
+    for smartgroup in smartgroups:
+        all_smartgroup_names.append(smartgroup['name'])
+
+    return all_smartgroup_names
+
+
+def list_all_devices_past(hasnt_checked_in_since: int) -> list:
+    list_past_checked_in_days = []
+    for comps in computers().json()['computers']:
+        comp_id = comps['id']
+        comp = computer_by_id(comp_id).json()
+        last_contact_time = comp['computer']['general']['last_contact_time']
+        if not last_contact_time:
+            pass
+        elif last_contact_time == "":
+            pass
+        else:
+            compare_dates = datetime.today() - datetime.strptime(last_contact_time.split(" ")[0], "%Y-%m-%d")
+            if 'day' not in str(compare_dates):
+                # less than 24 hours since check in
+                pass
+            else:
+                days_since_last_checkin = int(str(compare_dates).split(" ")[0])
+                if days_since_last_checkin >= hasnt_checked_in_since:
+                    list_past_checked_in_days.append(comp)
+                else:
+                    pass
+
+    return list_past_checked_in_days
+
+
+def checked_in_exceeds_date(device: dict, hasnt_checked_in_since: int) -> bool:
+    past_date = False
+    last_contact_time = device['computer']['general']['last_contact_time']
+    if not last_contact_time:
+        past_date = False
+    elif last_contact_time == "":
+        past_date = False
+    else:
+        compare_dates = datetime.today() - datetime.strptime(last_contact_time.split(" ")[0], "%Y-%m-%d")
+        if 'day' not in str(compare_dates):
+            # less than 24 hours since check in
+            past_date = False
+        else:
+            days_since_last_checkin = int(str(compare_dates).split(" ")[0])
+            if days_since_last_checkin >= hasnt_checked_in_since:
+                past_date = True
+
+
+    return past_date
+
+
+
+
